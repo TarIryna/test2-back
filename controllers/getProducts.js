6a56f92609d0dffd69e29c78aa31105d9d0b3fb5
TarIryna <tar86irina@gmail.com>
2022-09-12 10:49:32 +0300

    changed filePath

--- controllers/getProducts.js
+++ controllers/getProducts.js
@@ -1,14 +1,14 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const filePath = 'E:\\work\\test2-back\\data\\productsNames.json';
+
+const filePath = path.join(__dirname, 'productsNames.json');
 
 const getProducts = async (req, res) => {
   const products = [];
     const data = await fs.readFile(filePath)
     const result = JSON.parse(data);
     result.map(el => el.sub_categories.map(elem => products.push(elem.name)));
-    console.log(products)
     res.status(201).json({
         status: 'Ok',
         code: 201,
